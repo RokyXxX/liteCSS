10bbc689983a010903dca64d923e8dad4661c93f
RokyXxX <goofboiiahh@gmail.com>
2023-10-05 12:43:55 +0530

    Change Path (once again)...

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 
 export default {
-    input: "src/ts/index.ts",
+    input: "src/types/index.ts",
     output: {
         file: "dist/lite.bundle.js",
         format: "iife",
